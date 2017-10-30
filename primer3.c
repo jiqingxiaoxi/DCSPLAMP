a70d2728cd2fdd8602c9e1c8000e4b068b365736
Jia Ben <chenmodexiaoxi@126.com>
2017-10-30 16:23:07 +0800

    Add files via upload
    change-29: delete the reverse() subfunction

--- primer3.c
+++ primer3.c
@@ -2255,19 +2255,6 @@ double drawDimer(int *ps1,int *ps2,double H,double S,double Initdouble[],int Ini
         }
 }
 
-void reverse(unsigned char *s)
-{
-	int i,j;
-	char c;
-
-	for(i=0,j=strlen(s)-1;i<j;i++,j--)
-	{
-		c=s[i];
-		s[i]=s[j];
-		s[j]=c;
-	}
-}
-
 int symmetry_thermo(char seq[])
 {
 	char s,e;
